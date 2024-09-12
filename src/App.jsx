37e0f0ddd65b90c00ebfb6041c7b8177289e6658
Harshaljain04 <harshaljn04@gmail.com>
2024-09-12 23:16:41 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,32 @@
 import './App.css'
 
-import Dashboard from './pages/Dashboard/Dashboard'
-import CertificatePage from './pages/CertificatePage/CertificatePage'
-import InternshipPage from './pages/InternshipPage/InternshipPage'
-import Coursepage from './pages/CoursePage/CoursePage'
-import ProfilePage from './pages/ProfilePage/ProfilePage'
-import Chatbot from './pages/ChatBot/Chatbot.jsx'
-import Navbar from './components/Navbar/Navbar.jsx'
-import BlogPage from './pages/BlogPage/BlogPage'
-import LabsPage from './pages/Labs/LabsPage'
-import Loginpage from './pages/loginpage/loginpage'
+// import Dashboard from './pages/Dashboard/Dashboard'
+// import CertificatePage from './pages/CertificatePage/CertificatePage'
+// import InternshipPage from './pages/InternshipPage/InternshipPage'
+// import Coursepage from './pages/CoursePage/CoursePage'
+// import ProfilePage from './pages/ProfilePage/ProfilePage'
+// import Chatbot from './pages/ChatBot/Chatbot.jsx'
+// import Navbar from './components/Navbar/Navbar.jsx'
+// import BlogPage from './pages/BlogPage/BlogPage'
+// import LabsPage from './pages/Labs/LabsPage'
+// import Loginpage from './pages/loginpage/loginpage'
 
 
 // import Dashboard from './pages/Dashboard/Dashboard'
 // import CertificatePage from './pages/CertificatePage/CertificatePage'
-<<<<<<< HEAD
 // import InternshipPage from './pages/InternshipPage/InternshipPage'
 // import Coursepage from './pages/CoursePage/CoursePage'
 // import ProfilePage from './pages/ProfilePage/ProfilePage'
-=======
-//import InternshipPage from './pages/InternshipPage/InternshipPage'
-// import Coursepage from './pages/CoursePage/CoursePage'
->>>>>>> 9aad93868fbc83aa38029d1be8441605edcca161
 // import Chatbot from './pages/ChatBot/Chatbot.jsx'
 // import Navbar from './components/Navbar/Navbar.jsx'
 // import BlogPage from './pages/BlogPage/BlogPage'
-<<<<<<< HEAD
-import LabsPage from './pages/Labs/LabsPage'
+// import LabsPage from './pages/Labs/LabsPage'
 // import Loginpage from './pages/loginpage/loginpage'
-=======
-//import Landingpage from './pages/LandingPage/landingpage.jsx'
->>>>>>> 9aad93868fbc83aa38029d1be8441605edcca161
 
 
 function App() {
 
   return (
-<<<<<<< HEAD
-=======
-    //<InternshipPage />
->>>>>>> 9aad93868fbc83aa38029d1be8441605edcca161
     // <Loginpage />
     // <InternshipPage />
     // <Coursepage />
@@ -49,11 +36,7 @@ function App() {
     // <Navbar />
     // <BlogPage />
     // <ProfilePage />
-    <LabsPage />
-<<<<<<< HEAD
-=======
-    //<Landingpage/>
->>>>>>> 9aad93868fbc83aa38029d1be8441605edcca161
+    // <LabsPage />
   )
 }
 
