de9d1dfa41e4c64098723eeee57e884c4fbce4eb
Vansh41104 <vanshbhatnagar445@gmail.com>
2024-09-12 22:49:19 +0530

    modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,35 +11,9 @@ import BlogPage from './pages/BlogPage/BlogPage'
 import LabsPage from './pages/Labs/LabsPage'
 import Loginpage from './pages/loginpage/loginpage'
 
-
-// import Dashboard from './pages/Dashboard/Dashboard'
-// import CertificatePage from './pages/CertificatePage/CertificatePage'
-<<<<<<< HEAD
-// import InternshipPage from './pages/InternshipPage/InternshipPage'
-// import Coursepage from './pages/CoursePage/CoursePage'
-// import ProfilePage from './pages/ProfilePage/ProfilePage'
-=======
-//import InternshipPage from './pages/InternshipPage/InternshipPage'
-// import Coursepage from './pages/CoursePage/CoursePage'
->>>>>>> 9aad93868fbc83aa38029d1be8441605edcca161
-// import Chatbot from './pages/ChatBot/Chatbot.jsx'
-// import Navbar from './components/Navbar/Navbar.jsx'
-// import BlogPage from './pages/BlogPage/BlogPage'
-<<<<<<< HEAD
-import LabsPage from './pages/Labs/LabsPage'
-// import Loginpage from './pages/loginpage/loginpage'
-=======
-//import Landingpage from './pages/LandingPage/landingpage.jsx'
->>>>>>> 9aad93868fbc83aa38029d1be8441605edcca161
-
-
 function App() {
 
   return (
-<<<<<<< HEAD
-=======
-    //<InternshipPage />
->>>>>>> 9aad93868fbc83aa38029d1be8441605edcca161
     // <Loginpage />
     // <InternshipPage />
     // <Coursepage />
@@ -50,10 +24,6 @@ function App() {
     // <BlogPage />
     // <ProfilePage />
     <LabsPage />
-<<<<<<< HEAD
-=======
-    //<Landingpage/>
->>>>>>> 9aad93868fbc83aa38029d1be8441605edcca161
   )
 }
 
