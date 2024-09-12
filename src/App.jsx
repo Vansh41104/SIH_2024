36c139dd95dca74afedcf577e9d0aa0a14929964
Harshaljain04 <harshaljn04@gmail.com>
2024-09-12 22:35:11 +0530

    changes in image size

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,19 @@ import './App.css'
 // import Dashboard from './pages/Dashboard/Dashboard'
 // import CertificatePage from './pages/CertificatePage/CertificatePage'
 // import InternshipPage from './pages/InternshipPage/InternshipPage'
-import Coursepage from './pages/CoursePage/CoursePage'
-import ProfilePage from './pages/ProfilePage/ProfilePage'
+// import Coursepage from './pages/CoursePage/CoursePage'
+// import ProfilePage from './pages/ProfilePage/ProfilePage'
 // import Chatbot from './pages/ChatBot/Chatbot.jsx'
 // import Navbar from './components/Navbar/Navbar.jsx'
 // import BlogPage from './pages/BlogPage/BlogPage'
-<<<<<<< HEAD
 import LabsPage from './pages/Labs/LabsPage'
-=======
-import Loginpage from './pages/loginpage/loginpage'
->>>>>>> 2b72ded26dbfed3ee4630538491edbefad88ea8c
+// import Loginpage from './pages/loginpage/loginpage'
+
 
 function App() {
 
   return (
-    <Loginpage />
+    // <Loginpage />
     // <InternshipPage />
     // <Coursepage />
     // <Dashboard />
@@ -25,11 +23,8 @@ function App() {
     // <Chatbot />
     // <Navbar />
     // <BlogPage />
-<<<<<<< HEAD
-    <ProfilePage />
-=======
+    // <ProfilePage />
     <LabsPage />
->>>>>>> 7f45643069f9a2fb4885cfcf4078600111a2eb85
   )
 }
 
