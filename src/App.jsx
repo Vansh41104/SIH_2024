3ac26b1f29db4d2ef77820ab02bb5ff664af097c
Vansh41104 <vanshbhatnagar445@gmail.com>
2024-09-12 18:41:02 +0530

    added blog pages

--- src/App.jsx
+++ src/App.jsx
@@ -2,16 +2,18 @@
 import './App.css'
 import InternshipPage from './pages/InternshipPage/InternshipPage'
 // import Coursepage from './pages/CoursePage/CoursePage'
-import Chatbot from './pages/ChatBot/Chatbot.jsx'
+// import Chatbot from './pages/ChatBot/Chatbot.jsx'
 // import Navbar from './components/Navbar/Navbar.jsx'
+// import BlogPage from './pages/BlogPage/BlogPage'
 
 function App() {
 
   return (
-    // <InternshipPage />
+    <InternshipPage />
     // <Coursepage/>
-    <Chatbot/>
+    // <Chatbot/>
     // <Navbar/>
+    // <BlogPage />
   )
 }
 
---
+++ src/pages/BlogPage/BlogPage.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+import './BlogPage.css';
+import Navbar from '../../components/Navbar/Navbar';
+
+function BlogPage() {
+  return (
+
+    <div className="App">
+      <Navbar/>
+      <div className="container">
+        <div className="main-content">
+          <div className="user-card">
+            <h2>👤 user_01</h2>
+            <div className="main-post"></div>
+          </div>
+          <div className="other-users">
+            <div className="user-entry">👤 user_02</div>
+            <div className="user-entry">👤 user_20</div>
+            <div className="user-entry">👤 user_absd</div>
+            <div className="user-entry">👤 user_123</div>
+            <div className="user-entry">👤 user_123</div>
+            <div className="user-entry">👤 user_123</div>
+          </div>
+        </div>
+        <aside className="featured-posts">
+          <h3>Featured Posts</h3>
+          <div className="featured-post">👤 user_213</div>
+          <div className="featured-post">👤 user_542</div>
+          <div className="featured-post">👤 user_124</div>
+          <div className="featured-post">👤 user_124</div>
+          <div className="featured-post">👤 user_124</div>
+          <div className="featured-post">👤 user_124</div>
+          <div className="featured-post">👤 user_124</div>
+        </aside>
+      </div>
+    </div>
+  );
+}
+
+export default BlogPage;
