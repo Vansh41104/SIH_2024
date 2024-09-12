3b16a2843a14ff4bb98d0d1ece87fda7e351a746
Harshal Jain <99229107+Harshaljain04@users.noreply.github.com>
2024-09-12 16:20:38 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+import './App.css'
+import CoursePage from './Page'
+
+function App() {
+  return (
+    <CoursePage />
+     
+  )
+}
+
+export default App
