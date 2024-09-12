9aad93868fbc83aa38029d1be8441605edcca161
Vansh41104 <vanshbhatnagar445@gmail.com>
2024-09-12 22:44:00 +0530

    update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 
 import './App.css'
-<<<<<<< HEAD
+
 import Dashboard from './pages/Dashboard/Dashboard'
 import CertificatePage from './pages/CertificatePage/CertificatePage'
 import InternshipPage from './pages/InternshipPage/InternshipPage'
@@ -12,58 +12,32 @@ import BlogPage from './pages/BlogPage/BlogPage'
 import LabsPage from './pages/Labs/LabsPage'
 import Loginpage from './pages/loginpage/loginpage'
 
-=======
+
 // import Dashboard from './pages/Dashboard/Dashboard'
 // import CertificatePage from './pages/CertificatePage/CertificatePage'
-<<<<<<< HEAD
 //import InternshipPage from './pages/InternshipPage/InternshipPage'
 // import Coursepage from './pages/CoursePage/CoursePage'
-=======
-// import InternshipPage from './pages/InternshipPage/InternshipPage'
-// import Coursepage from './pages/CoursePage/CoursePage'
-// import ProfilePage from './pages/ProfilePage/ProfilePage'
->>>>>>> 194b47d535a2cb2cc9ac05ac9879a2e4bb70b22e
 // import Chatbot from './pages/ChatBot/Chatbot.jsx'
  //import Navbar from './components/Navbar/Navbar.jsx'
 // import BlogPage from './pages/BlogPage/BlogPage'
-<<<<<<< HEAD
 //import Landingpage from './pages/LandingPage/landingpage.jsx'
-=======
-import LabsPage from './pages/Labs/LabsPage'
-// import Loginpage from './pages/loginpage/loginpage'
 
->>>>>>> 194b47d535a2cb2cc9ac05ac9879a2e4bb70b22e
->>>>>>> 5337f6fc9842261a576cd0c2cd85de2876616819
 
 function App() {
 
   return (
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
     //<InternshipPage />
-=======
->>>>>>> 5337f6fc9842261a576cd0c2cd85de2876616819
     // <Loginpage />
     // <InternshipPage />
->>>>>>> 194b47d535a2cb2cc9ac05ac9879a2e4bb70b22e
     // <Coursepage />
     // <Dashboard />
     // <CertificatePage />
     // <Chatbot />
     // <Navbar />
     // <BlogPage />
-<<<<<<< HEAD
     // <ProfilePage />
     <LabsPage />
-=======
-<<<<<<< HEAD
     //<Landingpage/>
-=======
-    // <ProfilePage />
-    <LabsPage />
->>>>>>> 194b47d535a2cb2cc9ac05ac9879a2e4bb70b22e
->>>>>>> 5337f6fc9842261a576cd0c2cd85de2876616819
   )
 }
 
