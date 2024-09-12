8caf5b09e516fbb87e6e68c4e5a409a3618e76ab
Manas-tech <manastechnonjr@gmail.com>
2024-09-12 22:28:44 +0530

    landingpage

--- src/App.jsx
+++ src/App.jsx
@@ -2,22 +2,24 @@
 import './App.css'
 // import Dashboard from './pages/Dashboard/Dashboard'
 // import CertificatePage from './pages/CertificatePage/CertificatePage'
-// import InternshipPage from './pages/InternshipPage/InternshipPage'
+//import InternshipPage from './pages/InternshipPage/InternshipPage'
 // import Coursepage from './pages/CoursePage/CoursePage'
 // import Chatbot from './pages/ChatBot/Chatbot.jsx'
-// import Navbar from './components/Navbar/Navbar.jsx'
+ //import Navbar from './components/Navbar/Navbar.jsx'
 // import BlogPage from './pages/BlogPage/BlogPage'
+//import Landingpage from './pages/LandingPage/landingpage.jsx'
 
 function App() {
 
   return (
-    // <InternshipPage />
+    //<InternshipPage />
     // <Coursepage />
     // <Dashboard />
     // <CertificatePage />
     // <Chatbot />
     // <Navbar />
     // <BlogPage />
+    //<Landingpage/>
   )
 }
 
