e320f2dedd446b02071c3a8e30cdbd9100b6cba0
Vansh41104 <vanshbhatnagar445@gmail.com>
2024-09-12 22:34:04 +0530

    structured the code

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,21 @@
 
 import './App.css'
-// import Dashboard from './pages/Dashboard/Dashboard'
-// import CertificatePage from './pages/CertificatePage/CertificatePage'
-// import InternshipPage from './pages/InternshipPage/InternshipPage'
+import Dashboard from './pages/Dashboard/Dashboard'
+import CertificatePage from './pages/CertificatePage/CertificatePage'
+import InternshipPage from './pages/InternshipPage/InternshipPage'
 import Coursepage from './pages/CoursePage/CoursePage'
 import ProfilePage from './pages/ProfilePage/ProfilePage'
-// import Chatbot from './pages/ChatBot/Chatbot.jsx'
-// import Navbar from './components/Navbar/Navbar.jsx'
-// import BlogPage from './pages/BlogPage/BlogPage'
-<<<<<<< HEAD
+import Chatbot from './pages/ChatBot/Chatbot.jsx'
+import Navbar from './components/Navbar/Navbar.jsx'
+import BlogPage from './pages/BlogPage/BlogPage'
 import LabsPage from './pages/Labs/LabsPage'
-=======
 import Loginpage from './pages/loginpage/loginpage'
->>>>>>> 2b72ded26dbfed3ee4630538491edbefad88ea8c
+
 
 function App() {
 
   return (
-    <Loginpage />
+    // <Loginpage />
     // <InternshipPage />
     // <Coursepage />
     // <Dashboard />
@@ -25,11 +23,9 @@ function App() {
     // <Chatbot />
     // <Navbar />
     // <BlogPage />
-<<<<<<< HEAD
-    <ProfilePage />
-=======
-    <LabsPage />
->>>>>>> 7f45643069f9a2fb4885cfcf4078600111a2eb85
+    // <ProfilePage />
+    // <LabsPage />
+
   )
 }
 
