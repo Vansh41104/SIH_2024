81787a8d2be1d5e79c85a888e1b4e447cc9798db
arush75 <aarushmenaria75@gmail.com>
2024-09-14 10:43:14 +0530

    sign up page login auth updated and course page setup

--- src/pages/loginpage/itsparts/signup.jsx
+++ src/pages/loginpage/itsparts/signup.jsx
@@ -1,17 +1,25 @@
-import React, { useState } from 'react';
-
+import React, { useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Signup = ({ setActiveCard }) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
+  const navigate = useNavigate();
 
-  const handleSignup = () => {
-    if (password !== confirmPassword) {
-      alert('Passwords do not match');
-      return;
-    }
-    alert(`Signed up with ${email}`);
+  useEffect(() => {
+  
+    google.accounts.id.initialize({
+      client_id: '357073439690-d8mn6ge1eplpq9ont779k0ii2f28alav.apps.googleusercontent.com', 
+      callback: handleCredentialResponse,
+    });
+    google.accounts.id.renderButton(
+      document.getElementById('google-signin-button'),
+      { theme: 'outline', size: 'large' } 
+    );
+  }, []);
+
+  const handleCredentialResponse = (response) => {
+    console.log('Encoded JWT ID token: ' + response.credential);
+   
+    navigate('/dashboard');
   };
 
   return (
@@ -21,28 +29,23 @@ const Signup = ({ setActiveCard }) => {
       <input
         type="email"
         placeholder="balamia@gmail.com"
-        value={email}
-        onChange={(e) => setEmail(e.target.value)}
         name="email"
       />
       <label htmlFor="password">Password</label>
       <input
         type="password"
         placeholder="12345678"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
         name="password"
       />
       <label htmlFor="confirm-password">Confirm Password</label>
       <input
         type="password"
         placeholder="12345678"
-        value={confirmPassword}
-        onChange={(e) => setConfirmPassword(e.target.value)}
         name="confirm-password"
       />
+      <div id="google-signin-button"></div>
       <div className="last">
-        <button onClick={handleSignup}>Sign Up</button>
+        <button>Sign Up</button>
         <p>Already Have An Account? <span className="link" onClick={() => setActiveCard('login')}>Login</span></p>
       </div>
     </div>
